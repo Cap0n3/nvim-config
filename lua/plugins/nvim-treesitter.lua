6b307704dcca9498bcd3082038c06198ed47dc7a
Kalhal <cap0n3@protonmail.com>
2026-03-12 16:24:25 +0100

    Fixed error witch nvim-treesitter configs

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -12,94 +12,94 @@ return {
 		"nvim-treesitter/nvim-treesitter-textobjects",
 	},
 
-	config = function()
-		local configs = require("nvim-treesitter.configs")
-
-		configs.setup({
-			-- List of language parsers to install
-			ensure_installed = {
-				"bash",
-				"c",
-				"cpp",
-				"diff",
-				"go",
-				"gomod",
-				"gosum",
-				"gotmpl",
-				"html",
-				"htmldjango",
-				"javascript",
-				"jsdoc",
-				"json",
-				"jsonc",
-				"lua",
-				"luadoc",
-				"luap",
-				"markdown",
-				"markdown_inline",
-				"printf",
-				"python",
-				"query",
-				"regex",
-				"toml",
-				"tsx",
-				"typescript",
-				"vim",
-				"vimdoc",
-				"xml",
-				"yaml",
-			},
-
-			-- Don't install parsers synchronously (faster startup)
-			sync_install = false,
-
-			-- Enable syntax highlighting
-			highlight = { enable = true },
-
-			-- Enable better indentation
-			indent = { enable = true },
-
-			-- Enable incremental selection (expand/shrink selection by syntax nodes)
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "<C-space>", -- Start selection
-					node_incremental = "<C-space>", -- Expand to next node
-					scope_incremental = false, -- Disabled: expand to scope
-					node_decremental = "<bs>", -- Shrink to previous node
-				},
-			},
-
-			-- Enable text objects (navigation based on syntax tree)
-			textobjects = {
-				move = {
-					enable = true,
-					-- Move to the start of the next X
-					goto_next_start = {
-						["]f"] = "@function.outer",
-						["]c"] = "@class.outer",
-						["]a"] = "@parameter.inner",
-					},
-					-- Move to the end of the next X
-					goto_next_end = {
-						["]F"] = "@function.outer",
-						["]C"] = "@class.outer",
-						["]A"] = "@parameter.inner",
-					},
-					-- Move to the start of the previous X
-					goto_previous_start = {
-						["[f"] = "@function.outer",
-						["[c"] = "@class.outer",
-						["[a"] = "@parameter.inner",
-					},
-					-- Move to the end of the previous X
-					goto_previous_end = {
-						["[F"] = "@function.outer",
-						["[C"] = "@class.outer",
-						["[A"] = "@parameter.inner",
-					},
-				},
-			},
-		})
+	config = function(_, opts)
+		require("nvim-treesitter").setup(opts)
 	end,
+
+	opts = {
+		-- List of language parsers to install
+		ensure_installed = {
+			"bash",
+			"c",
+			"cpp",
+			"diff",
+			"go",
+			"gomod",
+			"gosum",
+			"gotmpl",
+			"html",
+			"htmldjango",
+			"javascript",
+			"jsdoc",
+			"json",
+			"jsonc",
+			"lua",
+			"luadoc",
+			"luap",
+			"markdown",
+			"markdown_inline",
+			"printf",
+			"python",
+			"query",
+			"regex",
+			"toml",
+			"tsx",
+			"typescript",
+			"vim",
+			"vimdoc",
+			"xml",
+			"yaml",
+		},
+
+		-- Don't install parsers synchronously (faster startup)
+		sync_install = false,
+
+		-- Enable syntax highlighting
+		highlight = { enable = true },
+
+		-- Enable better indentation
+		indent = { enable = true },
+
+		-- Enable incremental selection (expand/shrink selection by syntax nodes)
+		incremental_selection = {
+			enable = true,
+			keymaps = {
+				init_selection = "<C-space>", -- Start selection
+				node_incremental = "<C-space>", -- Expand to next node
+				scope_incremental = false, -- Disabled: expand to scope
+				node_decremental = "<bs>", -- Shrink to previous node
+			},
+		},
+
+		-- Enable text objects (navigation based on syntax tree)
+		textobjects = {
+			move = {
+				enable = true,
+				-- Move to the start of the next X
+				goto_next_start = {
+					["]f"] = "@function.outer",
+					["]c"] = "@class.outer",
+					["]a"] = "@parameter.inner",
+				},
+				-- Move to the end of the next X
+				goto_next_end = {
+					["]F"] = "@function.outer",
+					["]C"] = "@class.outer",
+					["]A"] = "@parameter.inner",
+				},
+				-- Move to the start of the previous X
+				goto_previous_start = {
+					["[f"] = "@function.outer",
+					["[c"] = "@class.outer",
+					["[a"] = "@parameter.inner",
+				},
+				-- Move to the end of the previous X
+				goto_previous_end = {
+					["[F"] = "@function.outer",
+					["[C"] = "@class.outer",
+					["[A"] = "@parameter.inner",
+				},
+			},
+		},
+	},
 }
